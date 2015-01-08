1fe78ec6608f732b479666667f7cb69190089d21
Jörg Kütemeier <jkuetemeier@jkuetemeier.com>
2015-01-08 01:26:25 +0100

    German Hello added

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ var express = require('express');
 var app = express();
 
 app.get('/hello.txt', function(req, res){
-	  res.send('Hello World');
+  res.send('Hello World');
 });
 
 app.get('/', function(req, res){
-	  res.send('Hello World from Node!');
+  res.send('Hello World from Node!');
 });
+
+app.get('/german', function(req, res) {
+  res.send('Hallo Welt');
+});
+
 var server = app.listen(3000, function() {
-	    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
 
